625c73a257b0b1a1200330696627c2007694c17a
12adrianmorente <adry_12_mg@hotmail.com>
2016-03-21 23:43:50 +0100

    all functions are implemented

--- bloqueLed/src/main.cpp
+++ bloqueLed/src/main.cpp
@@ -40,11 +40,46 @@ int main()
     apagar(b);
     cout << bloqueLedToString(b)<< endl;
 
-    cout << "\nAhora la animacion\nEjemplo 1 \n";
+    cout << "\nAhora la animacion\nEjemplo 1:\n";
+    encender(b);
+    cout << bloqueLedToString(b) << endl;
+    off(b, 7);
+    cout << bloqueLedToString(b) << endl;
+    on(b, 7);   off(b, 6);
+    cout << bloqueLedToString(b) << endl;
+    on(b, 6);   off(b, 5);
+    cout << bloqueLedToString(b) << endl;
+    on(b, 5);   off(b, 4);
+    cout << bloqueLedToString(b) << endl;
+    on(b, 4);   off(b, 3);
+    cout << bloqueLedToString(b) << endl;
+    on(b, 3);   off(b, 2);
+    cout << bloqueLedToString(b) << endl;
+    on(b, 2);   off(b, 1);
+    cout << bloqueLedToString(b) << endl;
+    on(b, 1);   off(b, 0);
+    cout << bloqueLedToString(b) << endl;
 
 
-    cout << "\nAhora la animacion\nEjemplo 2 \n";
-
+    cout << "\nAhora la animacion\nEjemplo 2: \n";
+    encender(b);
+    cout << bloqueLedToString(b) << endl;
+    off(b, 7);  off(b, 0);
+    cout << bloqueLedToString(b) << endl;
+    off(b, 6);  off(b, 1);
+    cout << bloqueLedToString(b) << endl;
+    off(b, 5);  off(b, 2);
+    cout << bloqueLedToString(b) << endl;
+    apagar(b);
+    cout << bloqueLedToString(b) << endl;
+    on(b, 4);   on(b, 3);
+    cout << bloqueLedToString(b) << endl;
+    on(b, 5);   on(b, 2);
+    cout << bloqueLedToString(b) << endl;
+    on(b, 6);   on(b, 1);
+    cout << bloqueLedToString(b) << endl;
+    on(b, 7);   on(b, 0);
+    cout << bloqueLedToString(b) << endl;
 
 
 }
