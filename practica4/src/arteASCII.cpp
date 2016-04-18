91fe6de406a766bb83cdab69acf3f20ffe459256
Adrian Morente <adrian95morente@gmail.com>
2016-04-18 09:21:25 +0200

    on the road to practica 4

--- practica4/src/arteASCII.cpp
+++ practica4/src/arteASCII.cpp
@@ -1,36 +1,46 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include "byte.h"
 #include "imagen.h"
 #include "pgm.h"
 using namespace std;
 
+void lee_linea(char c[], int tamano){
+    do{
+        cin.getline(c, tamano);
+    }while(c[0] == '\0');   //while(longitud(c)==0);
+}
+
 int main(){
     char nombre_imagen[131072];
+    char nombre_grises[60];
+    char nombre_txt[60];
     char arteASCII[131072];
-    char grises[10];
-    int n_grises=-1;
+    char aux[80];
+    ifstream entrada;
 
     Imagen origen;
 
     cout << "Introduce el nombre de la imagen de entrada: ";
     cin >> nombre_imagen;
 
-    cout << "\nVamos con los caracteres usados para el arte ASCII (0-255)." << endl;
-    do{
-        cout << "Introduce el numero de caracteres distintos usados (>=4 && <=10): ";
-        cin >> n_grises;
-    }while(n_grises<0 || n_grises >10);
+    cout << "Introduce el nombre del archivo que contiene los elementos de grises: ";
+    cin >> nombre_grises;
 
-    cout << "\nAhora introduce dichos caracteres (de uno en uno): ";
-    for(int i=0; i<n_grises; i++){
-        cout << "Caracter " << i << ": ";
-        cin >> grises[i];
+    cout << "Introduce el nombre del .txt de salida: ";
+    cin >> nombre_txt;
+
+    entrada.open(nombre_imagen);
+    if(entrada){
+        entrada >> lee_linea(aux, 80);    //ignoramos la primera linea
+        entrada >> n_cadenas;
+        while(entrada){
+            //entrada de datos
+        }
     }
-
-    if(!origen.leerImagen(nombre_imagen)){
-        cout << "Error leyendo imagen " << nombre_imagen << "...";
-        return 1;
+    else{
+        cerr << "Error de apertura del fichero...\n";
     }
 
     cout << "\nLa imagen en arte ASCII es:\n";
