5949038786948d55a73f714d16f3cf3335428f4a
Adrian Morente <adrian95morente@gmail.com>
2016-04-20 09:17:28 +0200

    on the road to practica 4

--- practica4/src/arteASCII.cpp
+++ practica4/src/arteASCII.cpp
@@ -13,42 +13,38 @@ void lee_linea(char c[], int tamano){
 }
 
 int main(){
-    char nombre_imagen[131072];
-    char nombre_grises[60];
-    char nombre_txt[60];
-    char arteASCII[131072];
-    char aux[80];
+    char imagen[100000];
+    char grises[100];
+    char salida[100000];
     ifstream entrada;
 
     Imagen origen;
 
-    cout << "Introduce el nombre de la imagen de entrada: ";
-    cin >> nombre_imagen;
+    cout << "imagen de entrada: ";
+    cin >> imagen;
+    cout << "fichero de cadenas: ";
+    cin >> grises;
+    cout << "fichero de salida: ";
+    cin >> salida;
 
-    cout << "Introduce el nombre del archivo que contiene los elementos de grises: ";
-    cin >> nombre_grises;
-
-    cout << "Introduce el nombre del .txt de salida: ";
-    cin >> nombre_txt;
-
-    entrada.open(nombre_imagen);
-    if(entrada){
-        entrada >> lee_linea(aux, 80);    //ignoramos la primera linea
-        entrada >> n_cadenas;
-        while(entrada){
-            //entrada de datos
-        }
-        entrada.close();
-    }
-    else{
-        cerr << "Error de apertura del fichero...\n";
-    }
-
-    cout << "\nLa imagen en arte ASCII es:\n";
-    if(origen.aArteASCII(grises, arteASCII, 131072))
-        cout << arteASCII;
-    else
-        cout << "La conversion no ha sido posible" << endl;
+    // entrada.open(imagen);
+    // if(entrada){
+    //     entrada >> lee_linea(aux, 80);    //ignoramos la primera linea
+    //     entrada >> n_cadenas;
+    //     while(entrada){
+    //         //entrada de datos
+    //     }
+    //     entrada.close();
+    // }
+    // else{
+    //     cerr << "Error de apertura del fichero...\n";
+    // }
+    //
+    // cout << "\nLa imagen en arte ASCII es:\n";
+    // if(origen.aArteASCII(grises, arteASCII, 131072))
+    //     cout << arteASCII;
+    // else
+    //     cout << "La conversion no ha sido posible" << endl;
 
     return 0;
 }
