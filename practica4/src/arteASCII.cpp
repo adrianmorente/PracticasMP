4f1f4006a240da74c04ce4dcbd5a06d3d4368a2c
Adrian Morente <adrian95morente@gmail.com>
2016-04-21 14:36:25 +0200

    fixing arteASCII.cpp

--- practica4/src/arteASCII.cpp
+++ practica4/src/arteASCII.cpp
@@ -6,18 +6,16 @@
 #include "pgm.h"
 using namespace std;
 
-void lee_linea(char c[], int tamano){
-    do{
-        cin.getline(c, tamano);
-    }while(c[0] == '\0');   //while(longitud(c)==0);
-}
-
 int main(){
-    char nombre_imagen[50000];
-    char nombre_grises[50];
-    char grises[200];
-    char img_salida[50000];
-    char arteASCII[50000];
+
+    const int MAX = 200000;
+    const int LONG = 60;
+
+    char nombre_imagen[LONG];     //nombre de la imagen de entrada
+    char nombre_grises[LONG];        //nombre del fichero de caracteres de entrada
+    char grises[MAX];                       //contenido del fichero de caracteres
+    char img_salida[MAX];         //contenido de la imagen de salida
+    char arteASCII[MAX];           //contenido de la imagen ASCII de salida
     ifstream entrada;
     Imagen origen;
 
@@ -26,24 +24,24 @@ int main(){
 
     cout << "imagen de entrada (imagenes/<nombre>.pgm): ";
     cin >> nombre_imagen;
-    cout << "fichero de cadenas: ";
+    cout << "fichero de cadenas (<nombre>.txt): ";
     cin >> nombre_grises;
-    cout << "fichero de salida: ";
+    cout << "fichero de salida (<nombre>.pgm): ";
     cin >> img_salida;
 
     ofstream salida(img_salida);
-
 
     entrada.open(nombre_grises);
 
     if(entrada){
         entrada.getline(aux, 80);    //ignoramos la primera linea
         entrada >> n_cadenas;        //capturamos el numero de cadenas de <nombre_grises>
+        cout << "nº cadenas: " << n_cadenas;
         if(!entrada){
             cerr << "Error de lectura del fichero de caracteres...\n";
         }
-        while(entrada){
-            entrada >> imagen;
+        while(!entrada.eof()){
+            entrada.getline() 
         }
         entrada.close();
     }
@@ -57,7 +55,7 @@ int main(){
     }
 
     cout << "\nLa imagen en arte ASCII es:\n";
-    if(origen.aArteASCII(grises, arteASCII, 50000))
+    if(origen.aArteASCII(grises, arteASCII, MAX))
         cout << arteASCII;
     else
         cout << "La conversion no ha sido posible" << endl;
