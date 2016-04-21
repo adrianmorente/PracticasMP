adc449be8d71a46cc5c90b42dce5c85ed7df8306
Adrian Morente <adrian95morente@gmail.com>
2016-04-21 18:28:33 +0200

    tryna fix ejercicio1

--- practica4/src/arteASCII.cpp
+++ practica4/src/arteASCII.cpp
@@ -8,31 +8,27 @@ using namespace std;
 
 int main(){
 
-    const int MAX = 200000;
+    //const int MAX = 200000;
     const int LONG = 60;
 
     char nombre_imagen[LONG];     //nombre de la imagen de entrada
     char nombre_grises[LONG];        //nombre del fichero de caracteres de entrada
-    char grises[MAX];                       //contenido del fichero de caracteres
     string img_salida;         //nombre parcial de las imagenes de salida
     ifstream entrada;
     Imagen origen;
 
     int n_cadenas; //numero de cadenas que hay en <nombre_grises>
     char aux[100];
+    int entero_aux;
 
     cout << "imagen de entrada (imagenes/<nombre>.pgm): ";
     cin >> nombre_imagen;
     cout << "fichero de cadenas (<nombre>.txt): ";
     cin >> nombre_grises;
-    cout << "fichero de salida (<nombre>.pgm): ";
+    cout << "fichero de salida: ";
     cin >> img_salida;
 
     entrada.open(nombre_grises);
-    char salida1[LONG];
-    char salida2[LONG];
-    char salida3[LONG];
-    char salida4[LONG];
 
     //Leemos el fichero de grises (comprobando errores) -->
     if(entrada){
@@ -42,14 +38,19 @@ int main(){
             cerr << "Error de lectura del fichero de caracteres...\n";
         }
         //Aqui leemos el resto del fichero y almacenamos en los ficheros de salida
-        //while(!entrada.eof()){
-        //}
-        entrada.getline(salida1, LONG);
-        entrada.getline(salida2, LONG);
-        entrada.getline(salida3, LONG);
-        entrada.getline(salida4, LONG);
-
+        int i=1;
+        string nombre;
+        while(!entrada.eof()){
+            //defino el nombre del archivo de salida
+            nombre = img_salida + to_string(i) + ".txt";
+            ofstream fsalida(nombre);
+            char auxiliar[LONG];
+            entrada.getline(auxiliar, LONG);
+            fsalida.close();
+            i++;
+        }
         entrada.close();
+        entero_aux = i;
     }
     else{
         cerr << "Error de apertura del fichero de caracteres...\n";
@@ -60,12 +61,18 @@ int main(){
         cout << "Error leyendo imagen " << nombre_imagen << "...";
         return 1;
     }
-    else{
-        if(origen.escribirImagen(salida1, LONG)){
-            cout << "yup";
+    else if(origen.leerImagen(nombre_imagen)){
+        for(int j=0; j<entero_aux; j++){
+            string faux;
+            faux = img_salida + to_string(entero_aux) + ".txt";
+            if(origen.escribirImagen(nombre_imagen, false)){
+                cout << "Imagen " << j << " creada correctamente." << endl;
+            }
+            else{
+                cout << "Error al escribir la imagen " << j << "..." << endl;
+            }
         }
     }
-
 
     return 0;
 }
